b6350f78d1fa8c1fb91054523e1f30358ea1fa2c
Mkepa <marcinkepa@hotmail.com>
2016-05-19 14:52:29 +0200

    Added directive

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -2,8 +2,17 @@ import {Component} from '@angular/core'
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+        <h2>Courses</h2>
+        {{ title }}
+        <ul>
+            <li *ngFor="#course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    `
 })
 export class CoursesComponent {
-    
+    title = "TItle of courses page";
+    courses = ["Course1", "Course2", "Course3"];
 }
