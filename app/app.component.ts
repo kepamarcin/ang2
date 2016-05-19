d4ea70e601c9b3ba3d3720850f74269cd6d600ee
Mkepa <marcinkepa@hotmail.com>
2016-05-19 13:14:42 +0200

    first componet

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,30 +1,9 @@
 import {Component} from '@angular/core';
-import {RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
+import {CoursesComponent} from './courses.component'
 
-import {NavBarComponent} from './navbar.component';
-import {HomeComponent} from './home.component';
-import {UsersComponent} from './users/users.component';
-import {UserFormComponent} from './users/user-form.component';
-import {PostsComponent} from './posts/posts.component';
-import {NotFoundComponent} from './not-found.component';
-
-@RouteConfig([
-	{ path: '/', name: 'Home', component: HomeComponent },
-	{ path: '/users', name: 'Users', component: UsersComponent },
-	{ path: '/users/:id', name: 'EditUser', component: UserFormComponent },
-	{ path: '/users/new', name: 'NewUser', component: UserFormComponent },
-    { path: '/posts', name: 'Posts', component: PostsComponent },
-    { path: '/not-found', name: 'NotFound', component: NotFoundComponent },
-	{ path: '/*other', name: 'Other', redirectTo: ['Home'] }
-])
 @Component({
     selector: 'my-app',
-    template: `
-        <navbar></navbar>
-        <div class="container">
-            <router-outlet></router-outlet>
-        </div>
-    `,
-    directives: [NavBarComponent, ROUTER_DIRECTIVES]
+    template: '<h1>Angular seed App</h1><courses></courses>' ,
+    directives: [CoursesComponent]  
 })
 export class AppComponent { }
